e48139656aa30009b216a140e53cb1d3c99fb161
shabbir642 <87113084+shabbir642@users.noreply.github.com>
2021-07-14 22:16:36 +0530

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -76,9 +76,9 @@ $app->configure('app');
 //     App\Http\Middleware\ExampleMiddleware::class
 // ]);
 
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+$app->routeMiddleware([
+    'auth' => App\Http\Middleware\Authenticate::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
@@ -91,8 +91,10 @@ $app->configure('app');
 |
 */
 $app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+$app->register(Illuminate\Notifications\NotificationServiceProvider::class);
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(\Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 $app->configure('services');
 $app->register(Illuminate\Mail\MailServiceProvider::class);
@@ -112,6 +114,9 @@ $app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
 | can respond to, as well as the controllers that may handle them.
 |
 */
+$app->routeMiddleware([
+    'cook' => App\Http\Middleware\CookieMiddleware::class,
+]);
 
 $app->router->group([
     'namespace' => 'App\Http\Controllers',

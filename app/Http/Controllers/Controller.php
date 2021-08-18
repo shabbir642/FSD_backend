00491ad2a389270027577fe48cbaaf90c725af64
shabbir642 <87113084+shabbir642@users.noreply.github.com>
2021-08-18 12:41:13 +0530

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     //
+    protected function respondWithToken($token,$user)
+    {
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() + 60
+        ], 200);
+    }
 }
